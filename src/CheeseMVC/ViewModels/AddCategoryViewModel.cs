c1febbfed49910ab47c90b140ca11a38bcbb09a3
Olga Sorokina <sorokinaog@icloud.com>
2019-01-01 19:35:19 -0500

    Assignment_CheeseMVC_Persistent

---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CheeseMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel : CheeseCategory
+    {
+        
+    }
+}
